b0762a699ebe136baa921dc8f21473a148a33db0
chenjinhe <>
2022-03-31 21:58:32 +0800

    log

--- opendfl-core/src/main/java/org/ccs/opendfl/core/utils/RequestParams.java
+++ opendfl-core/src/main/java/org/ccs/opendfl/core/utils/RequestParams.java
@@ -1,10 +1,12 @@
 package org.ccs.opendfl.core.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ccs.opendfl.core.config.OpendflConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public final class RequestParams {
     private RequestParams(){
 
@@ -15,7 +17,10 @@ public final class RequestParams {
     @Autowired
     public void setOpendflConfiguration(OpendflConfiguration opendflConfiguration) {
         RequestParams.opendflConfiguration = opendflConfiguration;
-        RequestParams.USER_ID=opendflConfiguration.getDefaultAttrName();
+        if(StringUtils.isNotBlank(opendflConfiguration.getDefaultAttrName())) {
+            RequestParams.USER_ID = opendflConfiguration.getDefaultAttrName();
+            log.info("-----USER_ID use default attrName={}", RequestParams.USER_ID);
+        }
     }
     /**
      * 用户ID
