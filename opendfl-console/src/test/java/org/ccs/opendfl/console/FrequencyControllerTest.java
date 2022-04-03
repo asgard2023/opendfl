b14ac40c6494dc48039b27b430eaccac250e6fc0
chenjh <>
2022-04-03 17:20:51 +0800

    simple test

--- opendfl-console/src/test/java/org/ccs/opendfl/console/FrequencyControllerTest.java
+++ opendfl-console/src/test/java/org/ccs/opendfl/console/FrequencyControllerTest.java
@@ -1,39 +1,41 @@
 package org.ccs.opendfl.console;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(value = "dev")
+@AutoConfigureMockMvc
+@WebAppConfiguration
 public class FrequencyControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
 
-    @Before
+    @BeforeEach
     public void setupMockMvc(){
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
     public void serverTime() throws Exception{
-        ResultActions result=mockMvc.perform(MockMvcRequestBuilders.get("/frequencyTest/serverTime") // 发出请求
-                        .contentType(MediaType.APPLICATION_JSON_UTF8) //期望的内容类型
-                        .accept(MediaType.APPLICATION_JSON_UTF8)) //期望接收的内容类型
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/frequencyTest/serverTime")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk()) //期望结果是200
-                .andDo(MockMvcResultHandlers.print()); // 期望把结果打印出来
+                .andReturn();
+        System.out.println(mvcResult);
     }
 }
