7c4c7b3e4c1e3de1085b7b016ba80d6ef12bd6f7
chenjh <>
2022-06-02 07:07:19 +0800

    CheckLogin

--- opendfl-mysql/src/main/java/org/ccs/opendfl/mysql/auth/CheckLogin.java
+++ opendfl-mysql/src/main/java/org/ccs/opendfl/mysql/auth/CheckLogin.java
@@ -1,4 +1,8 @@
 package org.ccs.opendfl.mysql.auth;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CheckLogin {
 }
